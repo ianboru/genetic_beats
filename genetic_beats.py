385a49517e8c50c6e009407be2ea73cd53257c76
lskenderi <lskenderi@digitalocean.com>
2018-06-10 15:56:39 -0400

    woohoo fixing things and making beats

--- genetic_beats.py
+++ genetic_beats.py
@@ -20,21 +20,26 @@ mutation_rate = .50
 print("beat_length: " + str(beat_length))
 sounds = []
 pg.mixer.set_num_channels(19)
-sounds.append(pg.mixer.Sound(pg.mixer.Sound("/Users/ianborukhovich/Projects/curiosity_driven_data_mining/genetic_beats/kick.wav")))
-sounds.append(pg.mixer.Sound(pg.mixer.Sound("/Users/ianborukhovich/Projects/curiosity_driven_data_mining/genetic_beats/snare.wav")))
-sounds.append(pg.mixer.Sound(pg.mixer.Sound("/Users/ianborukhovich/Projects/curiosity_driven_data_mining/genetic_beats/hi_hat.wav")))
+
+dirpath = os.path.abspath(os.path.dirname(__file__))
+
+# setting up sounds
+sounds.append(pg.mixer.Sound(pg.mixer.Sound(f"{dirpath}/kick.wav")))
+sounds.append(pg.mixer.Sound(pg.mixer.Sound(f"{dirpath}/snare.wav")))
+sounds.append(pg.mixer.Sound(pg.mixer.Sound(f"{dirpath}/hi_hat.wav")))
 #sounds.append(pg.mixer.Sound(pg.mixer.Sound("/Users/ianborukhovich/Projects/curiosity_driven_data_mining/genetic_beats/clave.wav")))
-sounds.append(pg.mixer.Sound(pg.mixer.Sound("/Users/ianborukhovich/Projects/curiosity_driven_data_mining/genetic_beats/G.wav")))
-sounds.append(pg.mixer.Sound(pg.mixer.Sound("/Users/ianborukhovich/Projects/curiosity_driven_data_mining/genetic_beats/C.wav")))
-sounds.append(pg.mixer.Sound(pg.mixer.Sound("/Users/ianborukhovich/Projects/curiosity_driven_data_mining/genetic_beats/Eb.wav")))
-sounds.append(pg.mixer.Sound(pg.mixer.Sound("/Users/ianborukhovich/Projects/curiosity_driven_data_mining/genetic_beats/F#.wav")))
-sounds.append(pg.mixer.Sound(pg.mixer.Sound("/Users/ianborukhovich/Projects/curiosity_driven_data_mining/genetic_beats/D.wav")))
+sounds.append(pg.mixer.Sound(pg.mixer.Sound(f"{dirpath}/G.wav")))
+sounds.append(pg.mixer.Sound(pg.mixer.Sound(f"{dirpath}/C.wav")))
+sounds.append(pg.mixer.Sound(pg.mixer.Sound(f"{dirpath}/Eb.wav")))
+sounds.append(pg.mixer.Sound(pg.mixer.Sound(f"{dirpath}/F#.wav")))
+# sounds.append(pg.mixer.Sound(pg.mixer.Sound(f"{dirpath}/D.wav")))
 
 num_sounds = len(sounds)
 sound_lengths = []
 for sound in sounds:
     sound_lengths.append(sound.get_length())
-print(sound_lengths)
+
+# every sound has a corresponding print output
 sound_names  = ['Kc','Sn','Hh', 'G','C','Eb','F#','D']
 
 def array_of_random_integers(length, max_int):
@@ -55,6 +60,7 @@ def create_inital_generation(sounds):
 
 def create_next_generation(last_generation, last_generation_fitness):
     next_generation = []
+    # pairwise comparison: all against all
     for a in range(0, len(last_generation)):
         for b in range(a, len(last_generation)): 
             #mate current pair to generate progeny
@@ -65,24 +71,26 @@ def create_next_generation(last_generation, last_generation_fitness):
     return next_generation
 
 def mate_current_pair(father,father_fitness, mother, mother_fitness):
-    current_generation_beats = []
+    current_mating_beats = []
     for x in range(0, num_progeny_per_mating):
         #choose member to donate each beat
         child_beat = []
+        # per note, chose mother or father note
         for y in range(0, len(father)):
             inhereted_beat = get_inherited_beat(father[y],father_fitness,mother[y], mother_fitness)
+            # if random chance is greater than mutation rate, keep inherited note, otherwise flip it / mutate
             mutated_beat = inhereted_beat if numpy.random.randint(0,101) > mutation_rate*100 else 1 - inhereted_beat
             child_beat.append(mutated_beat)
-        current_generation_beats.append(child_beat)
-    return current_generation_beats
+        current_mating_beats.append(child_beat)
+    return current_mating_beats
 
 def get_inherited_beat(father_beat,father_fitness,mother_beat, mother_fitness):
     percent_difference = abs((father_fitness - mother_fitness)/max(father_fitness,mother_fitness))
-    if numpy.random.randint(0,101) > 100*(percent_difference + 50):
+    # roll 100-sided die and if the number you get is in the upper half of the distribution that
+    if numpy.random.randint(0,101) > 100*(50 - percent_difference):
         return max(father_beat,mother_beat)
     else:
         return min(father_beat,mother_beat)
-    
 
 def identify_fit_progeny(last_generation):
     print("identifying fit members")
@@ -99,13 +107,15 @@ def identify_fit_progeny(last_generation):
             fitness = input("score beat\n")
         last_generation_fitness.append(int(fitness))
     saved_progeny_indices = find_top_n_indices(last_generation_fitness, num_saved_progeny_per_generation)
-    return [last_generation[i] for i in saved_progeny_indices], last_generation_fitness
+    # TODO: return only fitness scores of the fitest members of "last generation"
+    return [last_generation[i] for i in saved_progeny_indices], [last_generation_fitness[i] for i in saved_progeny_indices]
 
 def find_top_n_indices(list, n):
     top_n_indices = sorted(range(len(list)), key=lambda x: list[x])[-n:]
     return top_n_indices
 
 def play_beat(beat, num_loops):
+    # TODO: add ability to re-play beat
     stacked_beat = []
     try:
         for x in range(0, num_loops):
@@ -131,8 +141,10 @@ def play_beat(beat, num_loops):
                 print(beat_string)
             print("loop number " + str(x+1) + " of " +str(num_loops))
 
+    # TODO: fix KeyboardInterrupt
     except KeyboardInterrupt:
         print("interrupted")
+
 #evolve num_generations
 current_generation = create_inital_generation(sounds)
 
