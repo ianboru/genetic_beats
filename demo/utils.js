427e01b273ef587c78a087859ae37ee45dd33516
John Debs <johnthedebs@gmail.com>
2018-06-21 23:21:13 -0400

    Little more cleanup

--- demo/utils.js
+++ demo/utils.js
@@ -2,7 +2,7 @@ const arrayOfRandomIntegers = (numIntegers, arrayLength) => {
   let randomIntegerArray = []
   for (let i = 0; i < numIntegers; i++) {
     var randomInteger = Math.floor(Math.random() * arrayLength)
-    while(randomIntegerArray.indexOf(randomInteger) > -1){
+    while (randomIntegerArray.indexOf(randomInteger) > -1) {
       randomInteger = Math.floor(Math.random() * arrayLength)
     }
     randomIntegerArray.push(randomInteger)
@@ -10,29 +10,17 @@ const arrayOfRandomIntegers = (numIntegers, arrayLength) => {
   return randomIntegerArray
 }
 
-const arrayFromIndexList = (array,indexList) => {
-  let randomIntegerArray = []
-  console.log(array)
-  console.log(indexList)
-  console.log("final random ints")
-  indexList.forEach(
-    function(i){
-      console.log(i)
-      randomIntegerArray.push(array[i])
-  })
-  console.log(randomIntegerArray)
-  return randomIntegerArray
+const arrayFromIndexList = (array, indexList) => {
+  return indexList.map((i) => { return array[i] })
 }
 
-const findInJSON = (object,key,value) => {
+const findInJSON = (object, key, value) => {
   let result = {}
-  object.forEach(
-    function(element){
-      if(element[key] && element[key] == value ){
-        result = element
-      }
+  object.forEach((element) => {
+    if (element[key] && element[key] == value ) {
+      result = element
+    }
   })
-
   return result
 }
 
@@ -41,7 +29,7 @@ const mateCurrentPair = (mom,dad) => {
   console.log("mating current pair")
   console.log(mom)
   console.log(dad)
-  var percentDifference = 0
+  let percentDifference = 0
   const mutationRate = .05
 
   if(Math.max(dad["score"],mom["score"]) > 0){
@@ -50,8 +38,8 @@ const mateCurrentPair = (mom,dad) => {
   const inheritanceComparitor = 100*(.5 - percentDifference)
   const mutationComparitor = 100*mutationRate
 
-  var fittestBeat = {}
-  var weakestBeat = {}
+  let fittestBeat = {}
+  let weakestBeat = {}
   if (dad["score"] > mom["score"]) {
     fittestBeat = dad
     weakestBeat = mom
@@ -59,13 +47,13 @@ const mateCurrentPair = (mom,dad) => {
     fittestBeat = mom
     weakestBeat = dad
   }
-  var childBeat = []
+  let childBeat = []
   for (let noteIndex = 0; noteIndex < mom["beat"].length; noteIndex++) {
-    var randomInteger = Math.floor(Math.random() * 100)
-    var survivingNote = 0
-    if(randomInteger > inheritanceComparitor){
+    let randomInteger = Math.floor(Math.random() * 100)
+    let survivingNote = 0
+    if (randomInteger > inheritanceComparitor) {
       survivingNote = fittestBeat["beat"][noteIndex]
-    }else{
+    } else {
       survivingNote = weakestBeat["beat"][noteIndex]
     }
     randomInteger = Math.floor(Math.random() * 100)
