d5a29127d1136d973ed5085cd07c74b62e5b62fd
John Debs <johnthedebs@gmail.com>
2018-12-22 20:42:29 -0500

    Refactor Note component from beat.js to note.js

--- app/components/beat.js
+++ app/components/beat.js
@@ -21,6 +21,7 @@ import {
 import Player from "./player"
 
 import Button from "./button"
+import Note from "./note"
 import ConfigControl from "./configControl"
 
 import {allNotesInRange} from "../utils"
@@ -98,52 +99,6 @@ class GainSlider extends Component {
   }
 }
 
-
-const NoteWrapper = styled.div`
-  border-right: ${props => props.separator ? "1px solid white" : "0"};
-  display: inline-block;
-
-  &:last-child {
-    border-right: 0;
-  }
-`
-
-
-const StyledNote = styled.div`
-  background-color: ${props => props.active ? "pink" : props.on ? "red" : "gray" };
-  border-radius: 2px;
-  cursor: pointer;
-  display: inline-block;
-  height: 20px;
-  margin: 0 5px;
-  font-size: 15px;
-  width: 20px;
-
-  &:hover {
-    background-color: ${props => props.active ? "lightpink" : props.on ? "#FF6666" : "darkgray" };
-  }
-`
-
-
-@observer
-class Note extends Component {
-  render() {
-    const active = this.props.index == store.currentLitNote && store.playingCurrentBeat
-    const separator = this.props.index % 4 === 3
-
-    return (
-      <NoteWrapper separator={separator}>
-        <StyledNote
-          on          = {this.props.value === 1}
-          active      = {active}
-          onMouseDown = {this.props.onClick}
-          onMouseOver = {this.props.onMouseOver}
-          className   = "note"
-        >&nbsp;</StyledNote>
-      </NoteWrapper>
-    )
-  }
-}
 
 const trackNameStyles = {
   display       : "inline-block",
---
+++ app/components/note.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react"
+import { observer } from "mobx-react"
+import styled from "styled-components"
+
+import store from "../store"
+
+
+const NoteWrapper = styled.div`
+  border-right: ${props => props.separator ? "1px solid white" : "0"};
+  display: inline-block;
+
+  &:last-child {
+    border-right: 0;
+  }
+`
+
+const StyledNote = styled.div`
+  background-color: ${props => props.active ? "pink" : props.on ? "red" : "gray" };
+  border-radius: 2px;
+  cursor: pointer;
+  display: inline-block;
+  height: 20px;
+  margin: 0 5px;
+  font-size: 15px;
+  width: 20px;
+
+  &:hover {
+    background-color: ${props => props.active ? "lightpink" : props.on ? "#FF6666" : "darkgray" };
+  }
+`
+
+
+
+@observer
+class Note extends Component {
+  render() {
+    const active = this.props.index == store.currentLitNote && store.playingCurrentBeat
+    const separator = this.props.index % 4 === 3
+
+    return (
+      <NoteWrapper separator={separator}>
+        <StyledNote
+          on          = {this.props.value === 1}
+          active      = {active}
+          onMouseDown = {this.props.onClick}
+          onMouseOver = {this.props.onMouseOver}
+          className   = "note"
+        >&nbsp;</StyledNote>
+      </NoteWrapper>
+    )
+  }
+}
+
+
+export default Note
