9d5314dfe690be2896f3fb046ba32b8f73d92782
John Debs <johnthedebs@gmail.com>
2019-01-01 22:45:29 -0500

    Disable text selection on Note component to make dragging across less annoying

--- app/components/note.js
+++ app/components/note.js
@@ -26,6 +26,14 @@ const StyledNote = styled.div`
   font-size: 15px;
   width: 24px;
 
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
+
   &:hover {
     background-color: ${props => props.active ? "lightpink" : props.on ? "#FF6666" : "darkgray" };
   }
