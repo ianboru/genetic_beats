7e732c3b126d036670cf1cae06088b75a745de68
John Debs <johnthedebs@gmail.com>
2019-06-19 21:23:04 -0400

    Implement delete beat from lineage

--- app/stores/familyStore.js
+++ app/stores/familyStore.js
@@ -207,6 +207,16 @@ class FamilyStore {
   }
 
   @action deleteBeatFromLineage = (index) => {
+    if (this.currentBeatId === this.lineage[index]) {
+      if (this.lineage.length === 1) {
+        // TODO: Do something about this besides ignore user
+        return
+      } else if (index === this.lineage.length-1) {
+        this.currentBeatId = this.lineage[index-1]
+      } else {
+        this.currentBeatId = this.lineage[index+1]
+      }
+    }
     this.lineage.splice(index, 1)
   }
 
@@ -301,11 +311,7 @@ class FamilyStore {
 
   @action removeLastBeatFromLineage = () => {
     const lastBeatIndex = this.lineage.length - 1
-
-    if (this.currentBeatId === this.lineage[lastBeatIndex] && lastBeatIndex > this.lineage.length) {
-      this.currentBeatId = this.lineage[lastBeatIndex-1]
-    }
-    this.lineage.splice(lastBeatIndex)
+    this.deleteBeatFromLineage(lastBeatIndex)
   }
 
   @action removeLastBeatFromCurrentGen = () => {
